074ed07b4cceda2cc0aec1763f4350c3e6c1fdde
Asus <Asus@LAPTOP-DO6DL8JH>
2024-09-27 09:46:45 +0800

    Add project files.

---
+++ Basic Threading/FrmBasicThread.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using System;
+
+namespace Basic_Threading
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void StartThreads()
+        {
+            Thread ThreadA = new Thread(MyThreadClass.Thread1.threading);
+            Thread ThreadB = new Thread(MyThreadClass.Thread1.threading);
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            MessageBox.Show("Both threads have finished execution."); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartThreads();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Basic Threading/MyThreadClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basic_Threading
+{
+    public class MyThreadClass
+    {
+        public static class Thread1
+        {
+            public static void threading()
+            {
+                int attemptCount = 0;
+                while (attemptCount < 6)
+                {
+                    Thread Thread1 = Thread.CurrentThread;
+                    Console.WriteLine("Name of Thread: " + Thread1 + " = " + attemptCount);
+                    Thread.Sleep(1500);
+                    attemptCount++;
+                               
+                }
+
+            } 
+        }
+    }
+}
